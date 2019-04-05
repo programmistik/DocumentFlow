49ca7bbd63f72e5674e6f634228892221ccee27e
Enkeli __ <programmistik@hotmail.com>
2019-04-05 14:52:45 +0400

    add doc class

---
+++ DocumentFlow/Models/MyFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentFlow.Models
+{
+    public class MyFile
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileUri { get; set; }
+
+        public int DocId { get; set; }
+        public virtual Document Doc { get; set; }
+    }
+}
