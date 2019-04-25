b686b3bae104d4e46ea42a7a6a8ebf5469637b9c
Enkeli __ <programmistik@hotmail.com>
2019-04-25 17:32:33 +0400

    Add changes to Settings

--- DocumentFlow/ViewModels/SettingsPageViewModel.cs
+++ DocumentFlow/ViewModels/SettingsPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using DocumentFlow.Models;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using DocumentFlow.Models;
 using DocumentFlow.Services;
 using DocumentFlow.Views;
 using GalaSoft.MvvmLight;
@@ -8,30 +10,221 @@ using Google.Apis.Calendar.v3;
 using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace DocumentFlow.ViewModels
 {
-    public class SettingsPageViewModel : ViewModelBase
+    public class SettingsPageViewModel : ViewModelBase//, IDisposable
     {
         private readonly INavigationService navigationService;
         private readonly IMessageService messageService;
         private readonly AppDbContext db;
-        public SettingsPageViewModel(INavigationService navigationService, IMessageService messageService, AppDbContext db)
+        private readonly IPasswordSupplier passwordService;
+
+        private string ImageLink { get; set; }
+        private BitmapImage image;
+        public BitmapImage Image { get => image; set => Set(ref image, value); }
+        public ObservableCollection<FilterInfo> VideoDevices { get; set; }
+        private FilterInfo currentDevice;
+        public FilterInfo CurrentDevice { get => currentDevice; set => Set(ref currentDevice, value); }
+        public IVideoSource videoSource;
+
+        private string msg;
+        public string Msg { get => msg; set => Set(ref msg, value); }
+
+        private string msgColor;
+        public string MsgColor { get => msgColor; set => Set(ref msgColor, value); }
+
+        private string passCheckError;
+        public string PassCheckError { get => passCheckError; set => Set(ref passCheckError, value); }
+
+        private bool passwordConfirmation;
+        public bool PasswordConfirmation { get => passwordConfirmation; set => Set(ref passwordConfirmation, value); }
+
+
+        public SettingsPageViewModel(INavigationService navigationService, 
+                                    IMessageService messageService, 
+                                    AppDbContext db, 
+                                    IPasswordSupplier passwordService)
         {
             this.navigationService = navigationService;
             this.messageService = messageService;
             this.db = db;
+            this.passwordService = passwordService;
+
+            VideoDevices = new ObservableCollection<FilterInfo>();
+            GetVideoDevices();
+
+        }
+        public void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    var bi = new BitmapImage();
+                    bi.BeginInit();
+                    var ms = new MemoryStream();
+                    bitmap.Save(ms, ImageFormat.Bmp);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    bi.StreamSource = ms;
+                    bi.EndInit();
+
+                    bi.Freeze();
+                    Dispatcher.CurrentDispatcher.Invoke(() => Image = bi);
+                }
+            }
+            catch (Exception)
+            {
+                StopCamera();
+            }
+        }
+        private void StopCamera()
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.NewFrame -= video_NewFrame;
+            }
+            Image = null;
+        }
+
+        public void GetVideoDevices()
+        {
+            var devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in devices)
+                VideoDevices.Add(device);
+
+            if (VideoDevices.Any())
+                CurrentDevice = VideoDevices[0];
+        }
+        public void Dispose()
+        {
+            if (videoSource != null && videoSource.IsRunning)
+                videoSource.SignalToStop();
+
 
         }
 
+        private RelayCommand snapCommand;
+        public RelayCommand SnapCommand
+        {
+            get => snapCommand ?? (snapCommand = new RelayCommand(
+                () =>
+                {
+                    var encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(Image));
+                    ImageLink = Environment.CurrentDirectory + "\\snap.png";
+                    using (var fileStream = new FileStream(ImageLink, FileMode.Create))
+                    {
+                        encoder.Save(fileStream);
+                    }
+                    StopCamera();
+                    Image = new BitmapImage(new Uri(ImageLink));
+                }
+            ));
+        }
+        private RelayCommand uploadFileCommand;
+        public RelayCommand UploadFileCommand
+        {
+            get => uploadFileCommand ?? (uploadFileCommand = new RelayCommand(
+                () =>
+                {
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    openFileDialog.Filter = "All files (*.*)|*.*";
+
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        ImageLink = openFileDialog.FileName;
+                    }
+
+                    if (ImageLink != "")
+                    {
+                        StopCamera();
+                        Image = new BitmapImage(new Uri(ImageLink));
+                    }
+                }
+            ));
+        }
+
+        private RelayCommand<string> lostFocusCommand_tbUN;
+        public RelayCommand<string> LostFocusCommand_tbUN
+        {
+            get => lostFocusCommand_tbUN ?? (lostFocusCommand_tbUN = new RelayCommand<string>(
+                param =>
+                {
+                    if (!string.IsNullOrEmpty(param))
+                    {
+                        //var qwr = db.Users.Where(u => u.Login == param);
+                        //if (qwr.Any() == true)
+                        //{
+                        //    MsgColor = "Red";
+                        //    Msg = "This username is already taken";
+                        //}
+                        //else
+                        //{
+                        //    MsgColor = "Green";
+                        //    Msg = "You can use this username";
+                        //}
+                    }
+                }
+            ));
+        }
+
+        private RelayCommand<object> lostFocusCommand_pBox;
+        public RelayCommand<object> LostFocusCommand_pBox
+        {
+            get => lostFocusCommand_pBox ?? (lostFocusCommand_pBox = new RelayCommand<object>(
+                param =>
+                {
+                    var passwordContainer = param as IPasswordSupplier;
+                    if (passwordContainer != null)
+                    {
+                        var chk = passwordContainer.ConfirmPassword();
+                        if (chk)
+                        {
+                            PassCheckError = "";
+                            PasswordConfirmation = true;
+                        }
+                        else
+                        {
+                            PassCheckError = "Passwords don't match";
+                            PasswordConfirmation = false;
+                        }
+                    }
+                }
+            ));
+        }
+
+        private RelayCommand cancelCommand;
+        public RelayCommand CancelCommand
+        {
+            get => cancelCommand ?? (cancelCommand = new RelayCommand(
+                () =>
+                {
+                    // do nothing
+                    MsgColor = Msg = PassCheckError = "";
+                    //User = new User();
+                    StopCamera();
+                    navigationService.Navigate<LogInPageView>();
+                }
+            ));
+        }
         //Navigation
         //Upper Menu
         private RelayCommand gMain;
--- DocumentFlow/Views/SettingsPageView.xaml.cs
+++ DocumentFlow/Views/SettingsPageView.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using AForge.Video.DirectShow;
+using DocumentFlow.Services;
+using DocumentFlow.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +23,31 @@ namespace DocumentFlow.Views
     /// <summary>
     /// Interaction logic for SettingsPageView.xaml
     /// </summary>
-    public partial class SettingsPageView : UserControl
+    public partial class SettingsPageView : UserControl, IPasswordSupplier
     {
         public SettingsPageView()
         {
             InitializeComponent();
+        }
+
+        public SecureString GetPassword { get => pBox.SecurePassword; }
+
+        public bool ConfirmPassword()
+        {
+            return pBox.Password == pBox2.Password;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var data = DataContext as SettingsPageViewModel;
+            data.VideoDevices = new ObservableCollection<FilterInfo>();
+            data.GetVideoDevices();
+            if (data.CurrentDevice != null)
+            {
+                data.videoSource = new VideoCaptureDevice(data.CurrentDevice.MonikerString);
+                data.videoSource.NewFrame += data.video_NewFrame;
+                data.videoSource.Start();
+            }
         }
     }
 }
