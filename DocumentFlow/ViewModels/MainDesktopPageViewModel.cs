d552edc4d0d9208958879ea4476a76e4fa856ce0
sananyusibov99 <sananyusibov99@gmail.com>
2019-05-11 11:26:50 +0400

    small mistake fixed

--- DocumentFlow/ViewModels/MainDesktopPageViewModel.cs
+++ DocumentFlow/ViewModels/MainDesktopPageViewModel.cs
@@ -115,7 +115,7 @@ namespace DocumentFlow.ViewModels
                     navigationService.Navigate<ContactsPageView>();
                 }
             ));
-        #region NavigationCommands
+        #endregion
 
     }
 }
