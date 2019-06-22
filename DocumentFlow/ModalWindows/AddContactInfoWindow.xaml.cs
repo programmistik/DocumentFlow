5488087b3c75ebe345f29b4e2b19af92305e5897
sananyusibov99 <sananyusibov99@gmail.com>
2019-06-22 12:39:24 +0400

    e-mail and phone validation and fixed saving bug when closing a modal window

--- DocumentFlow/ModalWindows/AddContactInfoWindow.xaml.cs
+++ DocumentFlow/ModalWindows/AddContactInfoWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,21 +28,71 @@ namespace DocumentFlow.ModalWindows
             InfoCollection.SelectedIndex = 0;
         }
 
-       
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if ((c < '0' || c > '9') && c != '+' )
+                    return false;
+            }
+            return true;
+        }
+
+        bool EmailValidation(string str)
+        {
+            string email = str;
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (match.Success)
+                return true;
+            else
+                return false;
+        }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
-        {
-            Hide();
+        {            
+
+            if (InfoCollection.SelectedIndex.ToString() == "0" || InfoCollection.SelectedIndex.ToString() == "1")
+            {
+                if (IsDigitsOnly(InputValue.Text) == false)
+                {
+                    MessageBox.Show("Invalid phone");
+                }
+                else
+                {
+                    Hide();
+                }
+
+            }
+            if (InfoCollection.SelectedIndex.ToString() == "3")
+            {
+                if (EmailValidation(InputValue.Text) == false)
+                {
+                    MessageBox.Show("Invalid e-mail format");
+                }
+                else
+                {
+                    Hide();
+                }
+
+            }
+
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
+            InputValue.Text = null;
             Hide();
         }
 
         private void InfoCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            InputValue.Text = null;
         }
     }
 }
