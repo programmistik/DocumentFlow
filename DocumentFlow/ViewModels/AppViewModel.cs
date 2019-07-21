c09b988ba3c04ed5b60410a658959b3b77a0e758
sananyusibov99 <sananyusibov99@gmail.com>
2019-07-21 13:02:27 +0400

    Titles of all windows

--- DocumentFlow/ViewModels/AppViewModel.cs
+++ DocumentFlow/ViewModels/AppViewModel.cs
@@ -33,29 +33,55 @@ namespace DocumentFlow.ViewModels
                 var str = viewModel.ToString();
                 if (viewModel is LogInPageViewModel)
                     ApplicationTitle = Properties.Resources.LogIn;
-                else if (viewModel is AdminPanelPageViewModel)
-                    ApplicationTitle = "Administrator's panel";
+                else if (viewModel is AddEditEventPageViewModel)
+                    ApplicationTitle = "Add or Edit events";
+                else if (viewModel is AddNewsPageViewModel)
+                    ApplicationTitle = "Add or Edit News";
                 else if (viewModel is AddNewUserPageViewModel)
                 {
                     var vm = viewModel as AddNewUserPageViewModel;
-                    if(vm.ButtonOkContent == "Create")
+                    if (vm.ButtonOkContent == "Create")
                         ApplicationTitle = "Add new user";
                     else
                         ApplicationTitle = "Edit user";
                 }
-                else if (viewModel is NormativeInfoPageViewModel)
-                    ApplicationTitle = "Normative information";
-                else if (viewModel is ConstantsPageViewModel)
-                    ApplicationTitle = "Application's constants";
-                else if (viewModel is NewsListPageViewModel)
-                    ApplicationTitle = "News";
-                else if (viewModel is NewsPageViewModel)
-                    ApplicationTitle = "Add / Edit news post";
+                else if (viewModel is AdminPanelPageViewModel)
+                    ApplicationTitle = "Administrator's panel";
+                else if (viewModel is CalendarPageViewModel)
+                    ApplicationTitle = "Calendar";
                 else if (viewModel is ChangeMyPassPageViewModel)
                 {
                     var vm = viewModel as ChangeMyPassPageViewModel;
                     ApplicationTitle = "Change password for " + vm.CurrentUser.Login;
                 }
+                else if (viewModel is ComposeNewMailPageViewModel)
+                    ApplicationTitle = "New Mail";
+                else if (viewModel is ConstantsPageViewModel)
+                    ApplicationTitle = "Application's constants";
+                else if (viewModel is ContactsPageViewModel)
+                    ApplicationTitle = "Contacts";
+                else if (viewModel is CreateNewContactPageViewModel)
+                    ApplicationTitle = "Create new contact";
+                else if (viewModel is DocPageViewModel)
+                    ApplicationTitle = "Documents";
+                else if (viewModel is DocumentsPageViewModel)
+                    ApplicationTitle = "Documents";
+                else if (viewModel is GMailPageViewModel)
+                    ApplicationTitle = "Mail";
+                else if (viewModel is MainDesktopPageViewModel)
+                    ApplicationTitle = "Main Page";
+                else if (viewModel is NewsListPageViewModel)
+                    ApplicationTitle = "Add or Edit News";
+                else if (viewModel is NewsPageViewModel)
+                    ApplicationTitle = "News";
+                else if (viewModel is NormativeInfoPageViewModel)
+                    ApplicationTitle = "Normative information";
+                else if (viewModel is ReadMailPageViewModel)
+                    ApplicationTitle = "Read Mail";
+                else if (viewModel is SchedulePageViewModel)
+                    ApplicationTitle = "Schedule";
+                else if (viewModel is SettingsPageViewModel)
+                    ApplicationTitle = "Settings";
             }
             );
         }
